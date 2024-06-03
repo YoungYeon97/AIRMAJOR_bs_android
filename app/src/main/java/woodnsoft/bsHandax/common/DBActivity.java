d39ebbdac9329bd91a3794e525815e52113c0dc4
YoungYeon97 <python97@gmail.com>
2024-06-03 15:18:40 +0900

    edit

--- app/src/main/java/woodnsoft/bsHandax/common/DBActivity.java
+++ app/src/main/java/woodnsoft/bsHandax/common/DBActivity.java
@@ -9,40 +9,73 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class DBActivity extends BaseActivity implements DatabaseAdapterEvent {
 	private static final String TAG = "DBActivity";
+	private static final int PERMISSION_REQUEST_CODE = 1;
 
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		// Log.d(TAG,"1");
 		super.onCreate(savedInstanceState);
 
-		File fsdcard = Environment.getExternalStorageDirectory();  //SD CARD의 폴더경로
+		if (checkPermission()) {
+			initializeDatabase();
+		} else {
+			requestPermission();
+		}
+	}
+
+	private void initializeDatabase() {
+		File fsdcard = Environment.getExternalStorageDirectory();  // SD 카드 경로
 		File fdbpath = new File(fsdcard.getAbsolutePath() + File.separator + "WoodnSoft" + File.separator + "bsHandax");
 		if (!fdbpath.exists()){
 			Log.i(TAG,"Create Db directory : " + fdbpath.getAbsolutePath());
-
-			if(fdbpath.mkdirs()){
+			if (fdbpath.mkdirs()) {
 				Log.i(TAG, "OK");
-			}else{
+			} else {
 				Toast.makeText(getBaseContext(), "폴더 생성 실패!!!", Toast.LENGTH_SHORT).show();
 				Log.e(TAG, "NO");
 			}
 		}
 
-		DBH.dbPath = fdbpath.getAbsolutePath();              //DB 설치 경로
-		DBH.dbFile = "bsHandax2.db";                         //DB 파일명
-		DBH.dbPathFile = DBH.dbPath + File.separator + DBH.dbFile;  //DB 설치경로 + DB 파일명
+		DBH.dbPath = fdbpath.getAbsolutePath();  // DB 설치 경로
+		DBH.dbFile = "bsHandax2.db";  // DB 파일명
+		DBH.dbPathFile = DBH.dbPath + File.separator + DBH.dbFile;  // DB 경로 + 파일명
 
 		Log.i("TEST", "dbPathFile = " + DBH.dbPathFile);
 
-//		DBH.DB = openOrCreateDatabase(DBH.dbPathFile, MODE_WORLD_WRITEABLE, null);
-		DBH.DB = openOrCreateDatabase(DBH.dbPathFile, MODE_PRIVATE, null);
+		try {
+			DBH.DB = openOrCreateDatabase(DBH.dbPathFile, MODE_PRIVATE, null);
+		} catch (Exception e) {
+			Log.e(TAG, "데이터베이스 열기 실패: " + e.getMessage());
+		}
 
-		if(DBH.DB == null) DBH.openDatabase();
-
+		if (DBH.DB == null) DBH.openDatabase();
 	}
 
+	private boolean checkPermission() {
+		int result = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+		return result == PackageManager.PERMISSION_GRANTED;
+	}
+
+	private void requestPermission() {
+		ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+		switch (requestCode) {
+			case PERMISSION_REQUEST_CODE:
+				if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					initializeDatabase();
+				} else {
+					Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+				}
+				break;
+		}
+	}
 }
